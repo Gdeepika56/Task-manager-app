b3a571b07e6455edbc1fb7a06e1261e7073bd0ab
Gdeepika56 <dg1201@usnh.edu>
2024-12-02 18:34:27 -0500

    Add app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import api from './api/axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState({title: '', description: ''});
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try{
+        const response =await api.get('tasks/');
+        setTasks(response.data);
+      }catch(error){
+        console.error("error fetching tasks:", error);
+      }
+    };
+    fetchTasks();
+  }, []);
+
+  const handleAddTask = async () => {
+    try{
+      const response = await api.post('tasks/', newTask);
+      setTasks([...tasks, response.data]);
+      setNewTask({title: '', description: ''});
+    }catch(error) {
+      console.error("Error adding task:", error);
+    }
+  }
+
+  return(
+    <div>
+      <h1>Task Manager</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.title}: {task.description}
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          placeholder="title"
+          value={newTask.title}
+          onChange={(e) => setNewTask({...newTask, title: e.target.value})}
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          value={newTask.description}
+          onChange={(e) => setNewTask({...newTask, description: e.target.value})}
+          />
+          <button onClick ={handleAddTask} > Add Task</button>
+      </div>
     </div>
+    
+          
   );
 }
 
