59a2024bab4d77143bee51351dfa46429bd81645
Gdeepika56 <dg1201@usnh.edu>
2024-12-02 19:42:53 -0500

    add api urls

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import TaskListCreateView
+from .views import TaskDetail
 
 urlpatterns = [
     path("tasks/", TaskListCreateView.as_view(), name="task-list-create"),
+    path("tasks/<int:pk>/", TaskDetail.as_view(), name="task-detail"),
 ]
